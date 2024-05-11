12aec188f5e94faf2c6e9ab704da22526625d5ec
AndresveraXX <andres_9_09@hotmail.com>
2024-05-11 13:29:45 -0300

    pruebas

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -36,7 +36,7 @@ export class RoomsController {
     return this.roomsService.findOneByName(buildingId, name);
   }
 
-  @Patch(':buildingId/types/:roomId')
+  @Patch('/:roomId')
   update(@Param('buildingId') buildingId: string, @Param('roomId') roomId: string, @Body() updateRoomDto: UpdateRoomDto) {
     return this.roomsService.update(buildingId, roomId, updateRoomDto);
   }
