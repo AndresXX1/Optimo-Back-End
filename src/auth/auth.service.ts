e9fcfdaec68de95fef16b4cd1a5a83f41919fd07
AndresveraXX <andres_9_09@hotmail.com>
2024-05-16 13:11:36 -0300

    correccion del token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,19 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException(`password is wrong`);
     }
-    const payload = { email: user.email, role: user.role, name: user.name };
+    const payload = { 
+      email: user.email,
+       role: user.role,
+        name: user.name,
+         userId: user.id,
+          image: user.profilePicture,
+           country: user.country,
+            lastname: user.lastName,
+              age: user.age,
+                addres: user.address,
+                  gender: user.gender,
+                    phone: user.phone
+};
 
     return { access_Token: await this.jwtService.signAsync(payload) };
   }
